18e955c81dc307161b201df65cbd3e65f9c01441
xuemingming <xuemingming@zuowenzhitiao.com>
2021-03-22 15:30:08 +0800

    'firstProduct'

---
+++ src/test/java/com/xmm/alibaba/demo/SpringCloudAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmm.alibaba.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
